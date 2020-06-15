3133c2146dd90c59de75f650582f2c4b1f425429
Alexander Markov <ydmarkov@yandex.ru>
2020-06-16 02:02:34 +0300

    state draft

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
-    phpinfo();
+
+require('game.php');
+require_once ('pg.php');
+
+//$state = new State;
+//$state->movePiece(0, 0, 4, 4);
+//\PGDB\insertState('game-id-4', $state);
+
+$state = PGDB\getState('game-id-4');
+print_r($state);
+
 ?>
