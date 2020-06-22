2ad68fac86f79902e48011069493629236a19db5
Aleksandr Markov <ydmarkov@yandex.ru>
2020-06-22 12:27:21 +0000

    using granted user for pg

--- src/env.php
+++ src/env.php
@@ -11,8 +11,8 @@ $env = new Env;
 // --- SAMPLE ENVIRONMENT ---
 
 $env->pg = new PGCredentials();
-$env->pg->host = "host";
+$env->pg->host = "notebookdb.cdq8zesleh3d.us-east-2.rds.amazonaws.com";
 $env->pg->port = 5432;
 $env->pg->dbname = 'chess';
-$env->pg->username = 'user';
-$env->pg->password = 'password';
+$env->pg->username = 'chess_user';
+$env->pg->password = 'kjkszpj';
